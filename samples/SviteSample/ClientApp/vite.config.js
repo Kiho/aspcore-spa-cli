9d98b5e0fbad5c9a3badb871668a8220d4115628
Kiho <kihoc22@gmail.com>
2020-07-26 13:01:56 -0700

    Fix hmr, exclude _assets

--- samples/SviteSample/ClientApp/vite.config.js
+++ samples/SviteSample/ClientApp/vite.config.js
@@ -2,16 +2,3 @@ const svite = require('svite');
 module.exports = {
   plugins: [svite()],
 };
-
-
-// import svelte from 'vite-plugin-svelte';
-
-// const buildDir = '../wwwroot/dist';
-// const rollupOutputOptions = {
-//   dir: buildDir,
-// }
-
-// console.log('buildDir', buildDir);
-// module.exports = {
-//   plugins: [svelte()],
-// };
