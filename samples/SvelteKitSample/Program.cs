3415344879dec3d7b92432b43cac9537d747a512
Kiho <kihoc22@gmail.com>
2022-04-24 15:03:23 -0700

    Remove Razor project

--- samples/SvelteKitSample/Program.cs
+++ samples/SvelteKitSample/Program.cs
@@ -1,26 +1,63 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Jering;
+using Microsoft.AspNetCore.SpaServices;
+using SpaCliMiddleware;
 
-namespace SvelteKitSample
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+// Add Jering Node services
+builder.Services.ConfigureNodejsService();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+IHostEnvironment? hostEnvironment = app.Services.GetService<IHostEnvironment>();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+// app.UseResponseCompression();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+if (hostEnvironment != null)
+{
+    if (!hostEnvironment.IsDevelopment())
+    {
+        // Add 
+        app.UseNodejsService(hostEnvironment);
+        app.UseMiddleware<NodejsMiddleware>();
     }
 }
+
+app.UseEndpoints(endpoints => {
+    endpoints.MapControllers();
+
+    // Note: only use spacliproxy in development. 
+    // Production should use "UseSpaStaticFiles()"
+    endpoints.MapToSpaCliProxy(
+            "{*path}",
+            new SpaOptions { SourcePath = "Scripts" },
+            npmScript: hostEnvironment.IsDevelopment() ? "dev" : "",
+            port: /*default(int)*/ 8019, // Allow vite to find own port
+            regex: "SvelteKit v",
+            forceKill: true, // kill anything running on our webpack port
+            useProxy: true, // proxy webpack requests back through our aspnet server
+            runner: ScriptRunnerType.Npm
+        );
+});
+
+app.Run();
