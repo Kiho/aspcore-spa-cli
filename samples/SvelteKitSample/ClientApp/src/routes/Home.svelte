f582babcc5b07de6e72bd4437526bb840ea82125
Kiho <kihoc22@gmail.com>
2022-04-24 10:53:37 -0700

    Make copy from svite sample

---
+++ samples/SvelteKitSample/ClientApp/src/routes/Home.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Counter from '../components/Counter.svelte';
+	const world = 'world';
+</script>
+
+<style>
+  h1 {
+    color: orangered;
+  }
+</style>
+
+<h1>Hello {world}</h1>
+
+<Counter />
