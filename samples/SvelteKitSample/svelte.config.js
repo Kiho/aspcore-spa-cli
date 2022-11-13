a98ce3e2c3f8be11379afd3663f56f662553e6f8
Kiho <kihoc22@gmail.com>
2022-11-13 13:10:16 -0800

    Update dependency & config for migration

--- samples/SvelteKitSample/svelte.config.js
+++ samples/SvelteKitSample/svelte.config.js
@@ -17,11 +17,11 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html	
     files: {
 			assets: 'Assets',
-			hooks: 'Scripts/hooks',
+			// hooks: 'Scripts/hooks',
 			lib: 'Scripts/lib',
 			routes: 'Scripts/routes',
 			serviceWorker: 'Scripts/service-worker',
-			template: 'Scripts/app.html'
+			appTemplate: 'Scripts/app.html'
 		},
 		adapter: adapter({ out : 'build', precompress : false})
 	},
